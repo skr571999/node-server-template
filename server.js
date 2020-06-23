61e0a83385afaab15a85654ce1b033d59738045a
Sachin Kumar Rajput <skr571999@gmail.com>
2020-06-23 19:24:13 +0530

    - add db connection code - fix the /api error

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
+const mongoose = require("mongoose");
 const express = require("express");
+
 const app = express();
+
 // Create a .env file
 // require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/test";
+
+const connectDB = async () => {
+  try {
+    const con = await mongoose.connect(dbURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB Connection", con);
+  } catch (err) {
+    console.log("DB Error : ", err.message);
+  }
+};
+
+// connectDB();
 
 app.get("/", async (req, res) => {
   res.send({ message: "Server Running ..." });
 });
 
-app.get("/api", require("./routes"));
+app.use("/api", require("./routes"));
 
 app.listen(PORT, () => console.log(`Serving on ${PORT}`));
